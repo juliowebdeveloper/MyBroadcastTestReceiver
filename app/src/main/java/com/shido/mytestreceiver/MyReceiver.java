96cb8fe30f10154e177f2112b33ebce9eda7462f
Julio <juliowebdeveloper@hotmail.com>
2016-12-29 13:43:06 -0200

    Initial commit

---
+++ app/src/main/java/com/shido/mytestreceiver/MyReceiver.java
@@ -0,0 +1,16 @@
+package com.shido.mytestreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+    public MyReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Hello from app receiver", Toast.LENGTH_SHORT).show();
+    }
+}
